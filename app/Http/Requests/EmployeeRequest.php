e3607d694220e54515cc65e5b9d2e63515d69250
Mustafa Zahaak <me@mustafamuhammadi.mm54@gmail.com>
2021-01-13 04:05:36 +0430

    localization of Employee,User,Centers

--- app/Http/Requests/EmployeeRequest.php
+++ app/Http/Requests/EmployeeRequest.php
@@ -58,9 +58,34 @@ class EmployeeRequest extends FormRequest
 
     public function messages(){
         return [
-            'name.required'=>'نام ضروری است',
-            'father_name.required'=>'نام پدر ضروری است',
-            'id_details.volume.required_if'=>'جلد تذکره ضروری است'
+            'name.required'=>'نام ضروری میباشد',
+            'father_name.required'=>'نام پدر ضروری میباشد',
+            'grand_father_name.required'=>'نام پدر کلان ضروری میباشد',
+            'dob.required|date_format '=>'سال تولد ضروری بوده و فارمت تاریخ نیز باید درست باشد',
+            'gender.required'=>'جنسیت ضروری میباشد',
+            'marital_status.required'=>'حالت مدنی ضروری میباشد',
+            'contact_number.required'=>'شماره تماس ضروری میباشد',
+            'email.email'=>'ایمیل ضروری میباشد',
+            'center_id.required|exists'=>' وارد سازی مرکز ضروری میباشد طوریکه موجود باشد',
+            'id_type.required'=>'نوعیت ضروری میباشد',
+            'current_address.line_1.required'=>'کوچه سکونت فعلی ضروری میباشد',
+            'current_address.district.required'=>'ناحیه سکونت فعلی ضروری میباشد',
+            'current_address.province.required'=>'ولایت سکونت فعلی ضروری میباشد',
+            'permanent_address.line_1.required'=>'کوچه سکونت اصلی ضروری میباشد',
+            'permanent_address.district.required'=>'ناحیه سکونت اصلی ضروری میباشد',
+            'permanent_address.province.required'=>'ولایت سکونت اصلی ضروری میباشد',
+            'id_details.volume.required_if'=>'جلد تذکره ضروری میباشد',
+            'id_details.year.required_if'=>'تاریخ صدور ضروری میباشد',
+            'id_details.page.required_if'=>'صفحه تذکره ضروری میباشد',
+            'id_details.registration.required_if'=>'نمبر ثبت تذکره ضروری میباشد',
+            'id_details.sokok.required_if'=>'صکوک تذکره ضروری میباشد',
+            'id_details.nid_no.required_if'=>'نمبر تذکره ضروری میباشد',
+            'section.required'=>'بخش ضروری میباشد',
+            'directorate.required'=>'ریاست ضروری میباشد',
+            'position_code.required'=>'کود نمبر مقام ضروری میباشد',
+            'position_type.required'=>'نوعیت مقام ضروری میباشد',
+            'post_title.required'=>'عنوان بست ضروری میباشد',
+            'project_name.required'=>'عنوان بست ضروری میباشد' 
         ];
     }
 }
--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -13,7 +13,7 @@ class UserRequest extends FormRequest
      */
     public function authorize()
     {
-        return in_array($this->user()->role,['admin','manager']);
+        return in_array($this->user()->role, ['admin', 'manager']);
     }
 
     /**
@@ -24,10 +24,19 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'=>'required',
-            'role'=>'required',
-            'center_id'=>'required',
-            'email'=>'required|unique:users,email,'.$this->input('id').',id'
+            'name' => 'required',
+            'role' => 'required',
+            'center_id' => 'required',
+            'email' => 'required|unique:users,email,' . $this->input('id') . ',id'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'name.required' => 'اسم کاربری ضروری میباشد',
+            'role.required' => 'رول کاربری ضروری میباشد',
+            'center_id.required' => 'مرکز ضروری میباشد',
+            'email.required|unique' => 'ایمیل ضروری میباشد'
         ];
     }
 }
